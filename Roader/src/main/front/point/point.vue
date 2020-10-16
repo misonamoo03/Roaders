7421409aae29212ec44a11fe5bedf7d8c83fcd43
yogle <qjzjsldj@gmail.com>
2020-10-16 16:56:41 +0900

    Point.vue 완

--- Roader/src/main/front/point/point.vue
+++ Roader/src/main/front/point/point.vue
@@ -2,18 +2,18 @@
   <div id="app">
     <form>
       <input type="text" id="ruserPoint" v-bind:value="ruserPoint" readonly />
-      <button @click="plusP" type="button">입금버튼</button>
-      <button @click="minusP" type="button">출금버튼</button>
+      <button @click="plusPS" type="button">입금버튼</button>
+      <button @click="minusPS" type="button">출금버튼</button>
 
       <li
         v-for="review in reviews"
         :key="review.index"
-      >{{review.pointUpdate}} {{review.reason}} {{review.ruserId}} {{review.pointType}}{{review.ruserPoint}}</li>
+      >{{review.pointUpdate}}  {{review.reason}}  {{review.pointType}} {{review.ruserPoint}}₩</li>
 
       <div v-show="plusShow">
         <h5>입금하기</h5>
         <p>
-          <input type="text" id="ruserPoint" v-bind:value="ruserPoint" readonly />
+          <input type="text" id="ruserPoint" :value="varPoint" readonly />
         </p>
         <button type="button" v-on:click="setPlusRuserPoint(1000)">+1,000</button>
         <button type="button" v-on:click="setPlusRuserPoint(10000)">+10,000</button>
@@ -24,11 +24,11 @@
       <div v-show="minusShow">
         <h5>출금하기</h5>
         <p>
-          <input type="text" id="ruserPoint" v-bind:value="ruserPoint" readonly />
+          <input type="text" id="ruserPoint" :value="varPoint" readonly />
         </p>
-        <button type="button" v-on:click="setMinusRuserPoint(1000)">-1,000</button>
-        <button type="button" v-on:click="setMinusRuserPoint(10000)">-10,000</button>
-        <button type="button" v-on:click="setMinusRuserPoint(100000)">-100,000</button>
+        <button type="button" @click="setMinusRuserPoint(1000)">-1,000</button>
+        <button type="button" @click="setMinusRuserPoint(10000)">-10,000</button>
+        <button type="button" @click="setMinusRuserPoint(100000)">-100,000</button>
         <button @click="minusP" type="button">확인</button>
       </div>
     </form>
@@ -46,34 +46,43 @@ export default {
       minusShow: false,
       reviews: [],
       reason: "",
+      varPoint: 0,
       pointType: ""
     };
   },
-  watch: {
-    getPosts: function(event) {
-      let self=this;
-      if (self.ruserPoint <= 0) {
-        self.ruserPoint = 0;
+  computed: {
+       zeroPoint: function () {
+        if(this.ruserPoint < 0){
+            this.ruserPoint = 0;
+        }
+        return this.ruserPoint;
       }
-    }
   },
   methods: {
     setPlusRuserPoint: function(point) {
-      this.ruserPoint += point;
+      this.varPoint += point;
       return false;
     },
-    setMinusRuserPoint: function(point) {
-      this.ruserPoint -= point;
-      if (this.ruserPoint <= 0) {
-        this.ruserPoint = 0;
+    setMinusRuserPoint: function(point) {  
+      this.varPoint += point;
+      if(this.ruserPoint-this.varPoint<0){
+        alert("할 수 없습니다. 금액확인 후 다시해주세요.");
+        this.varPoint=0;
       }
       return false;
     },
-    plusP: function() {
+    plusPS: function() {
       let self = this;
       this.plusShow = !this.plusShow; // #2, #3
       this.minusShow = false; // #2, #3
-      axios({
+      this.varPoint = 0;
+    },
+    plusP: async function() {
+      let self = this;
+      let response;
+      this.plusShow = false;
+      this.ruserPoint+=this.varPoint;
+      response = await axios({
         url: "/pointUpdate",
         method: "post",
         data: {
@@ -81,12 +90,38 @@ export default {
           ruserPoint: self.ruserPoint
         }
       });
+      response = await axios({
+        url: "/reviewUpdate",
+        method: "post",
+        data: {
+          ruserId: self.ruserId,
+          reason: (self.reason = "입금"),
+          pointType: (self.pointType = "입금"),
+          ruserPoint: self.varPoint
+        }
+      });
+      response = await axios({
+        url: "/reviewHistory",
+        method: "post",
+        data: {
+          ruserId: self.ruserId
+        }
+      });
+      self.reviews = response.data;
+      this.varPoint=0;
     },
-    minusP: function() {
+    minusPS: function(){
       let self = this;
       this.minusShow = !this.minusShow; // #2, #3
       this.plusShow = false;
-      axios({
+      this.varPoint = 0;
+     },
+    minusP: async function() {
+      let self = this;
+      let response;
+      this.minusShow = false;
+      this.ruserPoint-=this.varPoint;
+      response = await axios({
         url: "/pointUpdate",
         method: "post",
         data: {
@@ -94,16 +129,25 @@ export default {
           ruserPoint: self.ruserPoint
         }
       });
-      axios({
+      response = await axios({
         url: "/reviewUpdate",
         method: "post",
         data: {
           ruserId: self.ruserId,
           reason: (self.reason = "출금"),
           pointType: (self.pointType = "출금"),
-          ruserPoint: self.ruserPoint
+          ruserPoint: self.varPoint
         }
       });
+      response = await axios({
+        url: "/reviewHistory",
+        method: "post",
+        data: {
+          ruserId: self.ruserId
+        }
+      });
+      self.reviews = response.data;
+      this.varPoint=0;
     },
     getPosts: async function(event) {
       let self = this,
@@ -135,7 +179,6 @@ export default {
   created: function() {
     let self = this;
     this.getPosts();
-    this.getW();
     console.log(self);
   }
 };
